d36887b88a1c29d9ff05df8381dcaa40e97bde2c
unknown user <giaada.donizetti@gmail.com>
2023-03-09 18:54:34 +0100

    Prima consegna.

--- src/Cognome.java
+++ src/Cognome.java
@@ -1,43 +1,124 @@
 import java.util.Scanner;
 
-// Chiamare la classe col proprio cognome
-public class MainClass
+public class Cognome
 {
-	static Scanner in = new Scanner(System.in);
+Scanner in = new Scanner(System.in);
 	
 	public static void main(String[] args)
 	{
-		while(true)
+		Scanner in = new Scanner(System.in);
+		System.out.println("ESERCITAZIONE 9");
+		//stampa titolo esercitazione
+		System.out.println("Effettuare una scelta");
+		System.out.println("1. Sistemazione array double");
+		System.out.println("2. Due sequenze");
+		int scelta=in.nextInt();
+		switch (scelta)
 		{
-			stampaMenu();
-			int scelta = Integer.parseInt(in.nextLine());
-			switch(scelta)
-			{
-				//aggiungere, togliere casi a seconda delle proprie scelte
-				case 1:
-					//Inserire metodo statico
-				break;
-				case 2:
-					//Inserire metodo statico
-				break;
-				case 3:
-					//Inserire metodo statico
-				break;
-				default:
-					System.out.println("Scelta errata, riprova!");
-
-			}
+		case 1:
+			PuntoUno();
+			break;
+		case 2:
+			DueSequenze();
+			break;
+		default:
+			Def();
+			break;
 		}
-	}
-	static void stampaMenu()
-	{
-		// Modificare il menù secondo le proprie scelte
-		System.out.println("1 - Es n. * - Titolo es. *");
-		System.out.println("2 - Es n. * - Titolo es. *");
-		System.out.println("3 - Es n. * - Titolo es. *");
-		System.out.println("4 - Es n. * - Titolo es. *");
-		System.out.println("5 - Es n. * - Titolo es. *");
+		
+		
+		
+		
+		
+		
 	}
 	
-	// Aggiungere qui i metodi statici relativi agli esercizi scelti
+	public static void PuntoUno()
+	{
+		Scanner in = new Scanner(System.in);
+		double [] Array = new double [6];
+		//creazione array di tipo double
+		double [] Finale = new double [Array.length /2];
+		//creazione secondo array di tipo double, con lunghezza pari alla metà
+		System.out.println("Inserire i valori double dell'array");
+		for (int i=0; i<Array.length; i++)
+		{
+			Array[i] = in.nextDouble();
+		}
+		//ciclo per riempire l'array
+		
+		for (int i = 0; i<Array.length -1; i+=2)
+		{
+			for (int j=1; j<=Array.length -1; i+=2)
+			{
+				for (int k=0; k<Finale.length; k++)
+				{
+					Finale[k] = (Array[i] * Array[j]);
+					//assegnazione
+					/*
+					 * alla locazione k, corrisponde il prodotto
+					 * delle locazioni i e j dell'array iniziale
+					 */
+				}
+			}
+		}
+		
+		
+		for(int i=0; i<Finale.length; i++)
+		{
+			System.out.println(Finale[i] + " ;");
+		}
+		//stampa a schermo dei valori dell'array
+
+	}
+	
+	public static void DueSequenze()
+	{
+		Scanner in = new Scanner(System.in);
+		String [] S1 = new String [5];
+		String [] S2 = new String [5];
+		
+		//dichiarazione due stringhe di array
+		System.out.println("riempire il primo array");
+		for(int i=0; i<S1.length; i++)
+		{
+			S1[i] =in.nextLine();
+		}
+		//riempimento primo array
+		
+		System.out.println("riempire il secondo array");
+		for(int i=0; i<S2.length; i++)
+		{
+			S2[i] =in.nextLine();
+		}
+		//riempimento secondo array
+		
+		
+		for (int i=0; i<=S1.length -1; i++)
+		{
+			if (S1[i] == S2[0] ||  S1[i] == S2[1] || S1[i] == S2[2] || S1[i] == S2[3] || S1[i] == S2[4])
+				/* 
+				 * se il valore della locazione i del primo array
+				 * è equivalente a quello delle altre locazioni del secondo array
+				 * stampa "ok"
+				 */
+			{
+				System.out.println("OK");
+			}
+			else
+			{	
+				System.out.println("NO");
+			}
+			//viceversa, stampa "no"
+		}
+		
+		
+	}
+	
+	public static void Def()
+	{
+		System.out.println("La scelta da lei effettuata non esiste");
+	}
+	//in caso venisse scelto un altro numero, stampa ciò
 }
+		
